a9e8683b1a5edc7445b4edf3285048f6920ae3eb
YeloHawPi <w.yelohawpi@gmail.com>
2017-10-24 20:12:12 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '330881821:AAFCyWJrUboyj0wALe8q6rBiWO1Rj5R8sd4';
+const bot = new TelegramBot(token, {polling: true});
+
+var notes = [];
+
+bot.onText(/\/reminder (.+) в (.+)/, (msg, match) => {
+    var userId = msg.from.id;
+    var text = match[1];
+    var time = match[2];
+
+    notes.push( { 'uid':userId, 'time':time, 'text':text } );
+
+	bot.sendMessage(userId, 'Хорошо! Я обязательно вам напомню!');
+});
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const resp = match[1];
+
+    bot.sendMessage(chatId, resp);
+});
+
+setInterval(function(){
+    for (var i = 0; i < notes.length; i++){
+        var curDate = new Date().getHours() + ':' + new Date().getMinutes();
+        if ( notes[i]['time'] == curDate ) {
+            bot.sendMessage(notes[i]['uid'], 'Напоминаю, что вы сейчас должны: '+ notes[i]['text']);
+            notes.splice(i,1);
+        }
+    }
+},1000); 
